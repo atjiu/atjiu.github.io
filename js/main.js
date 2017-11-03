5d1bc9fa9a1f2a74497a2446423fb49cea119c7c
tomoya <py2qiuse@gmail.com>
2017-11-03 16:32:34 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,60 +1,44 @@
-/**
- * some JavaScript code for this blog theme
- */
-/* jshint asi:true */
+$(function () {
+  var markdownToc = $("#markdown-toc");
+  markdownToc.hide();
+  var relatedPostsHtml = '';
+  var relatedPosts = $("#related_posts");
+  if(relatedPosts.text().length > 0) {
+    relatedPostsHtml = '<li><a href="#related_posts">' + relatedPosts.text() + '</a></li>';
+  }
+  $(".toc").html('<ul class="table-of-content">' +
+    markdownToc.html() +
+    '<li><a href="#comments">添加评论</a></li>' +
+    relatedPostsHtml +
+    '</ul>'
+  );
 
-/////////////////////////header////////////////////////////
-/**
- * clickMenu
- */
-(function() {
-    if (window.innerWidth <= 770) {
-        var menuBtn = document.querySelector('#headerMenu')
-        var nav = document.querySelector('#headerNav')
-        menuBtn.onclick = function(e) {
-            e.stopPropagation()
-            if (menuBtn.classList.contains('active')) {
-                menuBtn.classList.remove('active')
-                nav.classList.remove('nav-show')
-            } else {
-                nav.classList.add('nav-show')
-                menuBtn.classList.add('active')
-            }
+  $(".post img").click(function () {
+    var src = $(this).attr("src");
+    $("#bigImage").attr("src", src);
+    $("#toggleBigImageBtn").click();
+  });
+
+  var postRight = $(".post-right");
+  if(postRight) {
+    var postRightWidth = postRight.css('width');
+    postRight.affix({
+      offset: {
+        top: 60,
+        bottom: function () {
+          return (this.bottom = $('footer').outerHeight(true))
         }
-        document.querySelector('body').addEventListener('click', function() {
-            nav.classList.remove('nav-show')
-            menuBtn.classList.remove('active')
-        })
-    }
-}());
-
-
-//////////////////////////back to top////////////////////////////
-(function() {
-    var backToTop = document.querySelector('.back-to-top')
-    var backToTopA = document.querySelector('.back-to-top a')
-        // console.log(backToTop);
-    window.addEventListener('scroll',function () {
-
-
-        // 页面顶部滚进去的距离
-        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
-
-        if (scrollTop > 200) {
-            backToTop.classList.add('back-to-top-show')
-        } else {
-            backToTop.classList.remove('back-to-top-show')
-        }
-    })
-
-    // backToTopA.addEventListener('click',function (e) {
-    //     e.preventDefault()
-    //     window.scrollTo(0,0)
-    // })
-}());
-
-
-//////////////////////////hover on demo//////////////////////////////
-(function() {
-    var demoItems = document.querySelectorAll('.grid-item')
-}());
+      }
+    });
+    postRight.on('affix.bs.affix', function () {
+      postRight.css({
+        'top': '20px',
+        width: postRightWidth
+      });
+    });
+    postRight.on('affix-top.bs.affix', function () {
+      postRight.removeAttr('top');
+      postRight.removeAttr('width');
+    });
+  }
+});
