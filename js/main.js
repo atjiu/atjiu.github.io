79edaeef55772fdd73376501e9f0ba40a9cfc325
tomoya92 <py2qiuse@gmail.com>
2019-04-03 16:39:44 +0800

    修改样式

--- js/main.js
+++ js/main.js
@@ -1,63 +1,75 @@
-$(function () {
-  var markdownToc = $("#markdown-toc");
+$(function() {
+  var next_tip = localStorage.getItem('next_tip');
+  var adsbygoogle = $('.adsbygoogle').html();
+  var adblock = (adsbygoogle === undefined || adsbygoogle === '') && (!next_tip || new Date().getTime() > next_tip);
+  if (adblock) {
+    $('.tip-alert').show(700, 'swing');
+  }
+
+  $('.tip-alert button').click(function() {
+    var newTime = 7 * 24 * 60 * 60 * 1000;
+    var now = new Date().getTime();
+    localStorage.setItem('next_tip', now + newTime);
+  });
+
+  var markdownToc = $('#markdown-toc');
   var markdownTocHtml = '';
-  if(markdownToc.text().length > 0) {
+  if (markdownToc.text().length > 0) {
     markdownToc.hide();
     markdownTocHtml = markdownToc.html();
   }
-  $(".toc").html('<ul class="table-of-content">' +
-    markdownTocHtml +
-    '<li><a href="#comments">添加评论</a></li>' +
-    '</ul>'
-  );
+  $('.toc').html('<ul class="table-of-content">' + markdownTocHtml + '<li><a href="#comments">添加评论</a></li>' + '</ul>');
 
-  var postRight = $(".post-right");
-  if(postRight.length > 0) {
+  var postRight = $('.post-right');
+  if (postRight.length > 0) {
     var postRightWidth = postRight.css('width');
     postRight.css({
-      'position': 'fixed',
-      'width': postRightWidth
-    })
+      position: 'fixed',
+      width: postRightWidth
+    });
     var windowHeight = $(window).height();
     var postRightHeight = postRight.height();
     var offsetTop = postRight.offset().top;
-    if(postRightHeight + 40 > windowHeight) {
+    var toTopHeight = adblock ? 40 + 54 : 40;
+    if (postRightHeight + toTopHeight > windowHeight) {
       postRight.find('.panel-default').css({
-        'height': parseInt(windowHeight - 40) + 'px',
+        height: parseInt(windowHeight - toTopHeight) + 'px'
       });
       postRight.find('.panel-body').css({
-        'overflow': 'auto',
-        'height': parseInt(windowHeight - 40 - 42) + 'px',
-      })
+        overflow: 'auto',
+        height: parseInt(windowHeight - toTopHeight - 42) + 'px'
+      });
     }
-    if (offsetTop > 72) {
+    var offsetTopHeight = adblock ? 72 + 54 : 72;
+    if (offsetTop > offsetTopHeight) {
       postRight.css({
-        'top': '20px'
-      })
+        top: '20px'
+      });
     }
   }
   // back to top
-  var backToTop = $(".back-to-top");
-  $(window).scroll(function () {
+  var backToTop = $('.back-to-top');
+  $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
-    if (scrollTop >= 50) {
+    var flog = adblock ? scrollTop >= 104 : scrollTop >= 50;
+    if (flog) {
       postRight.css({
-        'position': 'fixed',
-        'top': '20px'
-      })
+        position: 'fixed',
+        top: '20px'
+      });
     } else {
       postRight.css({
-        'position': 'fixed',
-        'top': (70 - scrollTop) + 'px'
-      })
+        position: 'fixed',
+        top: adblock ? 124 - scrollTop + 'px' : 70 - scrollTop + 'px'
+      });
     }
-    if(scrollTop > 200) {
-      backToTop.addClass("back-to-top-show");
+    if (scrollTop > 200) {
+      backToTop.addClass('back-to-top-show');
     } else {
-      backToTop.removeClass("back-to-top-show");
+      backToTop.removeClass('back-to-top-show');
     }
-  })
-  backToTop.click(function () {
-    $("html,body").animate({scrollTop: 0}, 500);
-  })
+  });
+  backToTop.click(function() {
+    $('html,body').animate({ scrollTop: 0 }, 700);
+  });
 });
